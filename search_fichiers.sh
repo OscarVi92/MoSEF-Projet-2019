1391cd7cbcecd545a3c4fc1ab3a18ffe4c5cf88d
Scaroubelek <vidal.oscar92@gmail.com>
2019-11-28 15:25:43 +0100

    Commit branche2

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -2,9 +2,15 @@
 
 dateauj=$(date + '%d%B%Y')
 echo "Bienvenue $USER aujourd'hui nous sommes le $dateauj"
+
 #Demande à l'utilisateur quel repertoire l'interesse et affichage du repertoire
 read -p "Quel repertoire voulez ouvrir ?" directory
 echo -e "Vous avez choisi le repertoire" $directory
+
+#Question 6
+
 #Affichage de tous les fichiers du repertoire qui respecte le pattern associé
 ls $directory/$1
+
+
 
