875821bc24777c5ce030b549baaec65ce0c76886
Scaroubelek <vidal.oscar92@gmail.com>
2019-11-28 14:51:16 +0100

    Creation du Fichier

---
+++ search_fichiers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dateauj=$(date + '%d%B%Y')
+echo "Bienvenue $USER aujourd'hui nous sommes le $dateauj"
+#Demande à l'utilisateur quel repertoire l'interesse et affichage du repertoire
+read -p "Quel repertoire voulez ouvrir ?" directory
+echo -e "Vous avez choisi le repertoire" $directory
+#Affichage de tous les fichiers du repertoire qui respecte le pattern associé
+ls $directory/$1
+
